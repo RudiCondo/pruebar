35b050960cdbb412c54954c8689260e694b1e3f2
rudi <rudicondo2702@gmail.com>
2025-12-22 19:35:58 -0400

    Activandolo

--- frontend_github_pages/assets/js/config.js
+++ frontend_github_pages/assets/js/config.js
@@ -1,7 +1,7 @@
 // Configuración Global
 const CONFIG = {
     // API_BASE_URL: 'https://tu-proyecto-railway.app/api', // URL Producción (Descomentar y ajustar cuando despliegues)
-    API_BASE_URL: 'http://localhost:8000/api', // URL Local
+    API_BASE_URL: 'https://laravel-abmodel-production.up.railway.app/api', // URL Local   https://laravel-abmodel-production.up.railway.app/
     DEBUG: true
 };
 
